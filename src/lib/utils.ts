effa07a5a30f9cd69f1568d86ebc593d5d619cce
coco-lucas <lucasgcocos@gmail.com>
2025-07-30 22:35:27 -0300

    feat: tailwind-motions

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,7 +1,6 @@
 import { clsx, type ClassValue } from "clsx";
-import { useTranslation } from "react-i18next";
+import { useEffect } from "react";
 import { twMerge } from "tailwind-merge";
-import z from "zod";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
